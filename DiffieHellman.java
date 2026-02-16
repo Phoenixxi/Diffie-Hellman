7d6d7fb0202bca701cf09715b661686fa2e472a8
Phoenixxi <116411018+Phoenixxi@users.noreply.github.com>
2026-02-16 16:53:22 -0700

    I think its done

--- DiffieHellman.java
+++ DiffieHellman.java
@@ -23,9 +23,11 @@ public class DiffieHellman {
     String user2message;
     String user2EncryptedMessage;
     String user2Password;
-    java.math.BigInteger publicA;
-    java.math.BigInteger publicB;
-    java.math.BigInteger sharedSecret;
+    BigInteger publicA;
+    BigInteger publicB;
+    BigInteger sharedSecret;
+
+    int currentUser = 0; // Default=0, user1=1, user2=2
 
     DiffieHellman()
     {
@@ -43,17 +45,18 @@ public class DiffieHellman {
 
 
     private void user1() {
-        // START: choose prime number and generator
+        // Choose prime number and generator to start
         System.out.println("Hello User 1! To begin, we must choose a prime number and generator to be announced publically over the network. Would you like to have a prime number and generator chosen for you? (yes/no)");
         String response = scan.nextLine();
-        // User inputs yes. Get their prime and generator
+        // User inputs no. Get their prime and generator
         if (response.equalsIgnoreCase("no")) {
             System.out.println("Please enter a prime number:");
             primeNumber = scan.nextLong();
             System.out.println("Please enter a generator:");
             generator = scan.nextInt();
+            scan.nextLine(); 
         } 
-        // User inputs no. Randomly select a prime and generator from the map
+        // User inputs yes. Randomly select a prime and generator from the map
         else 
         {
             Random random = new Random();
@@ -71,22 +74,22 @@ public class DiffieHellman {
         // Acquire user 1 private key
         System.out.println("Please enter your private key. Note that your key will not be made public: ");
         user1privateKey = scan.nextInt();
+        scan.nextLine(); // Consume leftover newline
 
         // Compute public value
         publicA = ComputeBigX(user1privateKey);
         System.out.println("Your public value has been calculated. User 1 public value is " + publicA + ", this will be shared to everyone on the channel.");
         System.out.println("Please enter the message you want to encrypt:");
-        user1message = scan.next();
+        user1message = scan.nextLine();
         System.out.println("Thank you User 1! Now lets move on to User 2.");
-        user2();
 
     }
 
-    private java.math.BigInteger ComputeBigX(int secretVal)
+    private BigInteger ComputeBigX(int secretVal)
     {
-        java.math.BigInteger g = java.math.BigInteger.valueOf(generator);
-        java.math.BigInteger p = java.math.BigInteger.valueOf(primeNumber);
-        java.math.BigInteger e = java.math.BigInteger.valueOf(secretVal);
+        BigInteger g = BigInteger.valueOf(generator);
+        BigInteger p = BigInteger.valueOf(primeNumber);
+        BigInteger e = BigInteger.valueOf(secretVal);
 
         return g.modPow(e, p);
     }
@@ -94,47 +97,118 @@ public class DiffieHellman {
 
     private void user2()
     {   
-        // Acquire a password
+        // Acquire a first time password
         System.out.println("Hello User 2! Please enter a password: ");
-        user2Password = scan.next();
-        user2Password = scan.next();
+        user2Password = scan.nextLine();
         // Set private key
         System.out.println("Please enter your private key:");
         user2privateKey = scan.nextInt();
+        scan.nextLine();
         // Compute
         publicB = ComputeBigX(user2privateKey);
         System.out.println("Your public value has been calculated. User 2 public value is " + publicB + ", this will be shared to everyone on the channel.");
         ComputeSharedSecret(user2privateKey);
         // Encrypt user 1's message now that we know the shared secret
         EncryptMessage(user1message, sharedSecret, 1);
-        EncyptOrDecrypt();
+        System.out.println("(User 1's message has been encrypted to: " + user1EncryptedMessage + ")");
+        System.out.println("\nThank you users. You will be logged out now. Please log back in to encrypt or decrypt messages.");
+        Login();
 
     }
 
     private void ComputeSharedSecret(int secretVal)
     {
-        java.math.BigInteger p = java.math.BigInteger.valueOf(primeNumber);
-        java.math.BigInteger base = publicA;
-        java.math.BigInteger e = java.math.BigInteger.valueOf(secretVal);
+        BigInteger p = BigInteger.valueOf(primeNumber);
+        BigInteger base = publicA;
+        BigInteger e = BigInteger.valueOf(secretVal);
 
         sharedSecret = base.modPow(e, p);
+        System.out.println("Shared secret has been computed: " + sharedSecret);
 
+    }
+
+    private BigInteger TryComputeSharedSecret(int privateKey, BigInteger base)
+    {
+        BigInteger p = BigInteger.valueOf(primeNumber);
+        return base.modPow(BigInteger.valueOf(privateKey), p);
     }
 
     private void EncyptOrDecrypt()
     {
-        System.out.println("Would you like to decyrpt a message or send an encrypted message? Please write either \"decrypt\" or \"encrypt\"");
-        String response = scan.next();
+        System.out.println("\nWould you like to decrypt a message or send an encrypted message? Please write either \"decrypt\" or \"encrypt\"");
+        String response = scan.nextLine();
 
         if (response.equalsIgnoreCase("decrypt")) 
         {
+            System.out.println("Please enter your secret key to confirm your identity: ");
+            int privateKey = scan.nextInt();
+            scan.nextLine(); 
             
+            if(currentUser == 1)
+            {
+                if(user2EncryptedMessage == null){
+                    System.out.println("There are no messages from user 2 to decrypt");
+                    System.out.println("Logging out...");
+                    return;
+                }
+                BigInteger secretKey = TryComputeSharedSecret(privateKey, publicB);
+                System.out.println("Based on the private key you provided, the shared secret computed was: " + secretKey);
+                System.out.println("Decrypting message with shared secret: ");
+                System.out.println("...");
+                System.out.println("...");
+                System.out.println("...");
+                String message = DecryptMessage(user2EncryptedMessage, secretKey, 1);
+                System.out.println("The decrypted message is: " + message);
+                System.out.println("Logging out...");
+            }
+            else if(currentUser == 2)
+            {
+                if(user1EncryptedMessage == null){
+                    System.out.println("There are no messages from user 1 to decrypt");
+                    System.out.println("Logging out...");
+                    return;
+                }
+                BigInteger secretKey = TryComputeSharedSecret(privateKey, publicA);
+                System.out.println("Based on the private key you provided, the shared secret computed was: " + secretKey);
+                System.out.println("Decrypting message with shared secret: ");
+                System.out.println("...");
+                System.out.println("...");
+                System.out.println("...");
+                String message = DecryptMessage(user1EncryptedMessage, secretKey, 2);
+                System.out.println("The decrypted message is: " + message);
+                System.out.println("Logging out...");
+            }
+
+
         }
         else if(response.equalsIgnoreCase("encrypt"))
         {
-            System.out.println("Please enter the message you want to encrypt:");
-            user2message = scan.next();
-            System.out.println("Your message has been encrypted. You have been logged out");
+            
+            if(currentUser == 1) 
+            {
+                System.out.println("Please enter the message you want to encrypt:");
+                user1message = scan.nextLine();
+                System.out.println("Please enter your private key to encrypt the message:");
+                int privateKey = scan.nextInt();
+                scan.nextLine(); 
+                BigInteger secretKey = TryComputeSharedSecret(privateKey, publicB);
+                EncryptMessage(user1message, secretKey, 1);
+                System.out.println("Your message has been encrypted to: " + user1EncryptedMessage);
+                System.out.println("You have been logged out");
+                Login();
+            }
+            else if(currentUser == 2) {
+                System.out.println("Please enter the message you want to encrypt:");
+                user2message = scan.nextLine();
+                System.out.println("Please enter your private key to encrypt the message:");
+                int privateKey = scan.nextInt();
+                scan.nextLine(); 
+                BigInteger secretKey = TryComputeSharedSecret(privateKey, publicA);
+                EncryptMessage(user2message, secretKey, 2);
+                System.out.println("Your message has been encrypted to: " + user2EncryptedMessage);
+                System.out.println("You have been logged out");
+                Login();
+            } 
         }
         else
         {
@@ -143,9 +217,38 @@ public class DiffieHellman {
         }
     }
 
-    // private void TryDecrypt()
+    private String DecryptMessage(String message, BigInteger bigShift, int user)
+    {
+        int shift = (bigShift.mod(BigInteger.valueOf(ASCII_RANGE))).intValue();
+        shift = -shift;
 
-    private void EncryptMessage(String message, java.math.BigInteger bigShift, int user)
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < message.length(); i++) {
+            char ch = message.charAt(i);
+            
+            // Check if character is in printable ASCII range
+            if (ch >= ASCII_MIN && ch <= ASCII_MAX) {
+                // Normalize to 0-94 range, shift, wrap around, then denormalize
+                int normalized = ch - ASCII_MIN;
+                int shifted = (normalized + shift) % ASCII_RANGE;
+                
+                // Ensure no negative results
+                if (shifted < 0) {
+                    shifted += ASCII_RANGE;
+                }
+                
+                char newChar = (char) (shifted + ASCII_MIN);
+                result.append(newChar);
+            } else {
+                // Keep non-ascii characters unchanged
+                result.append(ch);
+            }
+        }
+        return result.toString();
+    }
+
+    private void EncryptMessage(String message, BigInteger bigShift, int user)
     {
         int shift = (bigShift.mod(BigInteger.valueOf(ASCII_RANGE))).intValue();
         StringBuilder result = new StringBuilder();
@@ -159,7 +262,7 @@ public class DiffieHellman {
                 int normalized = ch - ASCII_MIN;
                 int shifted = (normalized + shift) % ASCII_RANGE;
                 
-                // Handle negative modulo results
+                // Ensure no negative results
                 if (shifted < 0) {
                     shifted += ASCII_RANGE;
                 }
@@ -167,7 +270,7 @@ public class DiffieHellman {
                 char newChar = (char) (shifted + ASCII_MIN);
                 result.append(newChar);
             } else {
-                // Keep non-printable characters unchanged
+                // Keep non-ascii characters unchanged
                 result.append(ch);
             }
         }
@@ -181,16 +284,20 @@ public class DiffieHellman {
 
     private void Login()
     {
-        System.out.println("Please enter password to confirm your identity: ");
-        String password = scan.next();
+        System.out.println("\nPlease enter password to confirm your identity: ");
+        String password = scan.nextLine();
 
-        if (password.equalsIgnoreCase(user1Password))
+        if (password.equals(user1Password))
         {
-
+            currentUser = 1;
+            System.out.println("Logged in as User 1");
+            EncyptOrDecrypt();
         }
-        else if(password.equalsIgnoreCase(user2Password))
+        else if(password.equals(user2Password))
         {
-
+            currentUser = 2;
+            System.out.println("Logged in as User 2");
+            EncyptOrDecrypt();
         }
         else
         {
